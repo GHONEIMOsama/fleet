d9efd8cb733eff0568cfe831d0e72077e7f1d23e
ghoneim <osama.ghoneim.etu@univ-lille.fr>
2021-05-02 20:17:03 +0200

    FEAT: Handle EmptyResultDataAccessException

--- src/main/java/com/example/fleet/handlers/ErrorHandler.java
+++ src/main/java/com/example/fleet/handlers/ErrorHandler.java
@@ -2,6 +2,7 @@ package com.example.fleet.handlers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +18,7 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(ErrorHandler.class);
 
-    @ExceptionHandler(value = {EntityNotFoundException.class})
+    @ExceptionHandler(value = {EntityNotFoundException.class, EmptyResultDataAccessException.class})
     protected ResponseEntity<Object> entityNotFoundExceptionHandler(RuntimeException runtimeException, WebRequest webRequest) {
         LOG.info(runtimeException.getMessage());
         return handleExceptionInternal(runtimeException, runtimeException.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, webRequest);
