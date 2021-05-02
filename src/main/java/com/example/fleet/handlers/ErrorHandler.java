bcf9bc858cd5cb774d9587a57dcb6848f128cf4b
ghoneim <osama.ghoneim.etu@univ-lille.fr>
2021-05-02 18:56:17 +0200

    FEAT: implement the error handler

---
+++ src/main/java/com/example/fleet/handlers/ErrorHandler.java
@@ -0,0 +1,25 @@
+package com.example.fleet.handlers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+
+@ControllerAdvice
+public class ErrorHandler extends ResponseEntityExceptionHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ErrorHandler.class);
+
+    @ExceptionHandler(value = {EntityNotFoundException.class})
+    protected ResponseEntity<Object> entityNotFoundExceptionHandler(RuntimeException runtimeException, WebRequest webRequest) {
+        LOG.info(runtimeException.getMessage());
+        return handleExceptionInternal(runtimeException, runtimeException.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, webRequest);
+    }
+}
