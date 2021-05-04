465a6cc57377b353772f40c40009d78d51dad44f
ghoneim <osama.ghoneim.etu@univ-lille.fr>
2021-05-05 00:14:06 +0200

    FEAT: use ExpeditionService in expeditionController

--- src/main/java/com/example/fleet/controllers/ExpeditionController.java
+++ src/main/java/com/example/fleet/controllers/ExpeditionController.java
@@ -1,5 +1,6 @@
 package com.example.fleet.controllers;
 
+import com.example.fleet.exceptions.ExceedWeightException;
 import com.example.fleet.models.*;
 import com.example.fleet.repositories.CargoRepository;
 import com.example.fleet.repositories.DriverRepository;
@@ -7,8 +8,7 @@ import com.example.fleet.repositories.ExpeditionRepository;
 import com.example.fleet.repositories.TruckRepository;
 import com.example.fleet.requests.ExpeditionCreateRequest;
 import com.example.fleet.requests.ExpeditionPatchRequest;
-import com.example.fleet.requests.TruckCreateRequest;
-import com.example.fleet.requests.TruckPatchRequest;
+import com.example.fleet.services.ExpeditionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,20 +22,13 @@ import java.util.UUID;
 public class ExpeditionController {
 
     private final static String UNFOUND_EXPEDITION_MESSAGE = "Can't find expedition with id: %s";
-    private final static String UNFOUND_DRIVER_MESSAGE = "Can't find driver with id: %s";
-    private final static String UNFOUND_TRUCK_MESSAGE = "Can't find truck with id: %s";
-    private final static String UNFOUND_CARGO_MESSAGE = "Can't find cargo with id: %s";
 
     private final ExpeditionRepository expeditionRepository;
-    private final DriverRepository driverRepository;
-    private final TruckRepository truckRepository;
-    private final CargoRepository cargoRepository;
+    private final ExpeditionService expeditionService;
 
-    public ExpeditionController(ExpeditionRepository expeditionRepository, DriverRepository driverRepository, TruckRepository truckRepository, CargoRepository cargoRepository) {
+    public ExpeditionController(ExpeditionRepository expeditionRepository, ExpeditionService expeditionService) {
         this.expeditionRepository = expeditionRepository;
-        this.driverRepository = driverRepository;
-        this.truckRepository = truckRepository;
-        this.cargoRepository = cargoRepository;
+        this.expeditionService = expeditionService;
     }
 
 
@@ -53,51 +46,13 @@ public class ExpeditionController {
     }
 
     @PostMapping
-    public ResponseEntity<Expedition> create(@RequestBody @Valid ExpeditionCreateRequest expeditionCreateRequest) {
-        Expedition expedition = new Expedition();
-        Driver driver = driverRepository.findById(expeditionCreateRequest.getDriverId()).orElseThrow(() -> {
-           throw new EntityNotFoundException(String.format(UNFOUND_DRIVER_MESSAGE, expeditionCreateRequest.getDriverId()));
-        });
-        expedition.setDriver(driver);
-        Truck truck = truckRepository.findById(expeditionCreateRequest.getTruckId()).orElseThrow(() -> {
-            throw new EntityNotFoundException(String.format(UNFOUND_TRUCK_MESSAGE, expeditionCreateRequest.getTruckId()));
-        });
-        expedition.setTruck(truck);
-        Cargo cargo = cargoRepository.findById(expeditionCreateRequest.getCargoId()).orElseThrow(() -> {
-           throw new EntityNotFoundException(String.format(UNFOUND_CARGO_MESSAGE, expeditionCreateRequest.getCargoId()));
-        });
-        expedition.setCargo(cargo);
-        expeditionRepository.save(expedition);
-        return new ResponseEntity<>(expedition, HttpStatus.CREATED);
+    public ResponseEntity<Expedition> create(@RequestBody @Valid ExpeditionCreateRequest expeditionCreateRequest) throws ExceedWeightException {
+        return new ResponseEntity<>(expeditionService.create(expeditionCreateRequest), HttpStatus.CREATED);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<Void> update(@PathVariable UUID id, @RequestBody ExpeditionPatchRequest expeditionPatchRequest) {
-        Expedition expedition = expeditionRepository.findById(id).orElseThrow(() -> {
-           throw new EntityNotFoundException(String.format(UNFOUND_EXPEDITION_MESSAGE, id));
-        });
-        if (expeditionPatchRequest.getCargoId() != null) {
-            Cargo cargo = cargoRepository.findById(expeditionPatchRequest.getCargoId()).orElseThrow(() -> {
-               throw new EntityNotFoundException(String.format(UNFOUND_CARGO_MESSAGE, expeditionPatchRequest.getCargoId()));
-            });
-            expedition.setCargo(cargo);
-        }
-        if (expeditionPatchRequest.getTruckId() != null) {
-            Truck truck = truckRepository.findById(expeditionPatchRequest.getTruckId()).orElseThrow(() -> {
-                throw new EntityNotFoundException(String.format(UNFOUND_TRUCK_MESSAGE, expeditionPatchRequest.getTruckId()));
-            });
-            expedition.setTruck(truck);
-        }
-        if (expeditionPatchRequest.getDriverId() != null) {
-            Driver driver = driverRepository.findById(expeditionPatchRequest.getDriverId()).orElseThrow(() -> {
-               throw new EntityNotFoundException(String.format(UNFOUND_DRIVER_MESSAGE, expeditionPatchRequest.getDriverId()));
-            });
-            expedition.setDriver(driver);
-        }
-        if (expeditionPatchRequest.getStartTime() != null) {
-            expedition.setStartTime(expeditionPatchRequest.getStartTime());
-        }
-        expeditionRepository.save(expedition);
+        expeditionService.update(id, expeditionPatchRequest);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
