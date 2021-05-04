02a374a805927647d8125caa67f4a30cb13e69e4
ghoneim <osama.ghoneim.etu@univ-lille.fr>
2021-05-05 00:11:59 +0200

    CHORE: create expeditionService

---
+++ src/main/java/com/example/fleet/services/ExpeditionService.java
@@ -0,0 +1,16 @@
+package com.example.fleet.services;
+
+import com.example.fleet.exceptions.ExceedWeightException;
+import com.example.fleet.models.Expedition;
+import com.example.fleet.requests.ExpeditionCreateRequest;
+import com.example.fleet.requests.ExpeditionPatchRequest;
+
+import java.util.UUID;
+
+public interface ExpeditionService {
+
+    Expedition create(ExpeditionCreateRequest expeditionCreateRequest) throws ExceedWeightException;
+
+    void update(UUID id, ExpeditionPatchRequest expeditionPatchRequest);
+
+}
